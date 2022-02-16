5ef6ab32c605f27b66ef24b779624a0973530a4b
pierwill <pierwill@users.noreply.github.com>
2022-02-15 20:52:37 -0600

    sketches

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,93 @@
-#![allow(unused_imports)]
 #![allow(dead_code)]
 
 use std::io::{stdin, stdout, Write};
 
-use termion::event::{Event, Key};
+use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
+const Q: &str = "press q to exit";
+
 fn main() {
+    // let mut stdin = termion::async_stdin().keys();
     let stdin = stdin();
-    let mut stdout = stdout();
+    let mut stdout = stdout().into_raw_mode().unwrap();
 
     write!(
         stdout,
-        "{}{}q to exit",
+        "{}{}{}",
         termion::clear::All,
-        termion::cursor::Goto(1, 1)
+        termion::cursor::Goto(1, 1),
+        Q,
     )
     .unwrap();
     stdout.flush().unwrap();
 
-    for c in stdin.events() {
-        let evt = c.unwrap();
-        match evt {
-            Event::Key(Key::Char('q')) => break,
-            Event::Key(Key::Char('f')) => write!(stdout, "{}f", termion::clear::All,).unwrap(),
-            Event::Key(Key::Char('d')) => write!(stdout, "{}d", termion::clear::All,).unwrap(),
+    write!(stdout, "{}", termion::cursor::Goto(1, 2)).unwrap();
+
+    for c in stdin.keys() {
+        match c.unwrap() {
+            Key::Ctrl('c') | Key::Char('q') => break,
+            Key::Char('f') => {
+                // record data
+                write!(stdout, "f{}", termion::cursor::Goto(1, 2)).unwrap()
+            }
+            Key::Char('d') => {
+                // record data
+                write!(stdout, "d{}", termion::cursor::Goto(1, 2)).unwrap()
+            }
             _ => {}
         }
         stdout.flush().unwrap();
     }
+
+    let mut x = FiveGram::new();
+    x.push('a');
+    x.push('b');
+    x.push('b');
+    x.push('b');
+    x.push('b');
+    println!("{x:?}");
 }
+
+fn update_model_f() {
+    // function updateModelF (fivegram) {
+    //   return function (letter) {
+    //     var fg = model[fivegram]
+    //     if (!fg) {
+    //       model[fivegram] = { f: 0, d: 0 }
+    //     }
+    //     model[fivegram][letter]+=1
+    //     return
+    //   }
+    // }
+}
+fn predict_next_letter() {
+    // function predictNextLetter (fivegram) {
+    //   var m = model[fivegram]
+    //   if (!m)
+    //     return 'f'
+    //   if (m.f > m.d)
+    //     return 'f'
+    //   return 'd'
+    // }
+}
+fn predict(input: char) {
+    // function predict (inputS) {
+    //   var lastSix = inputS.slidingWindow(6,6)
+    //   return lastSix.map(s => {
+    //     var fiveGram = _.slice(s, 0,5).join('')
+    //     // predict next value
+    //     var prediction = predictNextLetter(fiveGram)
+    //     //make a fn to update model after i see real value
+    //     var updateModel = updateModelF(fiveGram)
+    //     // get the next letter now
+    //     var last = _.last(s)
+    //     // update my model with it (HACK SIDE-EFFECTY)
+    //     updateModel(last)
+    //     return [prediction, last]
+    //   })
+    // }
+}
+
+type FiveGram = arrayvec::ArrayVec<char, 5>;
