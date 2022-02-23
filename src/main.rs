7fd96986c66c80bc24565fac55fe699faae6f56a
pierwill <pierwill@users.noreply.github.com>
2022-02-23 15:17:01 -0600

    Better prediction implementation

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,6 @@ use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
-mod mean;
-
 const Q: &str = "press q to exit";
 
 fn main() {
@@ -86,36 +84,25 @@ fn main() {
     let mut out = String::new();
     out.push_str(format!("key history: {all_keys:?}").as_str());
     out.push_str(format!("model: {:#?}", model.map).as_str());
+    out.push_str(format!("number of entries in map: {}", model.map.len()).as_str());
     std::fs::write("out", out.as_bytes()).expect("oops");
 }
 
 /// A map giving the score for each fivegram.
-#[derive(Debug, Clone)]
+#[derive(Default, Debug, Clone)]
 struct Model {
     map: HashMap<Vec<char>, Score>,
-}
-
-impl Default for Model {
-    fn default() -> Self {
-        Self {
-            map: HashMap::default(),
-        }
-    }
 }
 
 /// For each fivegram, f is the number
 /// of times f was pressed next, and likewise
 /// for d.
-#[derive(Debug, Clone)]
+#[derive(Default, Debug, Clone)]
 struct Score {
     f: i32,
     d: i32,
 }
 
-impl Default for Score {
-    fn default() -> Self {
-        Self { f: 0, d: 0 }
-    }
 }
 
 fn update_model(m: Model, all_keys: Vec<char>) -> Model {
@@ -128,19 +115,18 @@ fn update_model(m: Model, all_keys: Vec<char>) -> Model {
         .take(1)
         .collect::<Vec<_>>()[0];
     let lastfive: Vec<char> = all_keys.into_iter().rev().take(5).collect();
-    let current_score: Option<&Score> = model.map.get(&lastfive);
 
     if lastfive.len() < 5 {
         return Model::default();
     }
 
+    let current_score: Option<&Score> = model.map.get(&lastfive);
     let mut new_score = current_score.unwrap_or(&Score::default()).clone();
     if last == 'f' {
         new_score.f += 1;
     } else if last == 'd' {
         new_score.d += 1;
     }
-    println!("let's insert!");
     model.map.insert(lastfive, new_score);
 
     model
@@ -149,20 +135,19 @@ fn update_model(m: Model, all_keys: Vec<char>) -> Model {
 fn predict(m: Model, all_keys: Vec<char>) -> char {
     let fivegram: Vec<char> = all_keys.into_iter().rev().take(5).collect();
     if m.map.is_empty() {
-        println!("map is empty");
         return 'f';
     }
-
     let current_score = m.map.get(&fivegram);
 
-    if current_score.is_some() {
-        if current_score.unwrap().f > current_score.unwrap().d {
-            return 'f';
-        } else {
-            return 'd';
-        }
+    match current_score {
+        Some(s) if s.f > s.d => 'f',
+        Some(s) if s.d > s.f => 'd',
+        // If we have no score, yet, predict 'f'.
+        None => 'f',
+        _ => unreachable!(),
     }
+}
 
-    println!("i'm jsut saying f");
-    'f'
+fn is_correct(predicted: char, got: char) -> bool {
+    predicted == got
 }
